a8b6642225a4771dc453bf688dc70cf41d9763e2
OmidSargazi <om.sargazi@gmail.com>
2024-03-17 23:44:18 +0330

    complete programs section

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import Hero from "./components/Hero/Hero";
 import Navbar from "./components/Navbar/Navbar";
+import Programs from "./components/Programs/Programs";
 
 function App() {
   return (
     <div>
       <Navbar />
       <Hero />
+      <div className="container">
+        <Programs />
+      </div>
     </div>
   );
 }
