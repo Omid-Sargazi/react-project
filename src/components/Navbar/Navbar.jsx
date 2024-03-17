44a0977d92aff40572b1babcd900b552c5e319ca
OmidSargazi <om.sargazi@gmail.com>
2024-03-17 22:36:14 +0330

    create navbar

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import "./Navbar.css"
+import logo from "../../assets/logo.png"
+const Navbar = () => {
+  return (
+    <nav className='container'>
+      
+      <img className='logo' src={logo} alt=''/>
+      <ul>
+        <li>Home</li>
+        <li>Program</li>
+        <li>About us</li>
+        <li>Campus</li>
+        <li>Testimonials </li>
+        <li> <button className='btn'>Contact us</button></li>
+      </ul>
+    </nav>
+  )
+}
+
+export default Navbar
