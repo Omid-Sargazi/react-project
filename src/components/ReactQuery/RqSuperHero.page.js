d8579dc98eeb91c12140b21721c509d495f6331f
OmidSargazi <om.sargazi@gmail.com>
2024-04-09 02:15:16 +0330

    Query Cache

--- src/components/ReactQuery/RqSuperHero.page.js
+++ src/components/ReactQuery/RqSuperHero.page.js
@@ -7,10 +7,12 @@ const fetchSuperHeros = () => {
 };
 
 export default function RqSuperHero() {
-  const { isLoading, data, isError, error } = useQuery(
+  const { isLoading, data, isError, error, isFetching } = useQuery(
     "super-heroes",
     fetchSuperHeros
   );
+
+  console.log({ isLoading, isFetching });
 
   if (isLoading) {
     return <h2>Loading....</h2>;
