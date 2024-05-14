d1d406b0b146316491080b6608907d9fce3660e6
OmidSargazi <om.sargazi@gmail.com>
2024-05-15 00:53:33 +0330

    React Hooks Fetching data with useEffect Part 2

--- src/components/DataFetching.js
+++ src/components/DataFetching.js
@@ -4,26 +4,30 @@ import { useState } from "react";
 import axios from "axios";
 
 export default function DataFetching() {
-  const [data, setData] = useState([]);
+  //   const [data, setData] = useState([]);
+  const [post, setPost] = useState({});
+  const [id, setId] = useState(1);
 
   useEffect(() => {
     axios
-      .get("https://jsonplaceholder.typicode.com/posts")
+      .get(`https://jsonplaceholder.typicode.com/posts/${id}`)
       .then((res) => {
-        console.log(res.data[0].title);
-        setData(res.data);
+        // console.log(res.data[0].title);
+        setPost(res.data);
       })
       .then((error) => {
         console.log(error);
       });
-  }, []);
+  }, [id]);
   return (
     <div>
-      <ul>
-        {data.map((data) => (
+      <input type="text" value={id} onChange={(e) => setId(e.target.value)} />
+      <div>{post.title}</div>
+      {/* <ul>
+        {post.map((data) => (
           <li key={data.id}>{data.title}</li>
         ))}
-      </ul>
+      </ul> */}
     </div>
   );
 }
