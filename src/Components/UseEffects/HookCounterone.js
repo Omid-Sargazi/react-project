37e0873220452ad1428d4d859706ac9c34011481
OmidSargazi <om.sargazi@gmail.com>
2024-05-27 23:15:39 +0330

    React Hooks Conditionally run effects

--- src/Components/UseEffects/HookCounterone.js
+++ src/Components/UseEffects/HookCounterone.js
@@ -4,14 +4,22 @@ import HookCounterTwo from "../HookCounterTwo";
 
 export default function HookCounterone() {
   const [count, setCOunt] = useState(0);
+  const [name, setName] = useState("");
+
+  const handleInput = (e) => {
+    setName(e.target.value);
+  };
 
   useEffect(() => {
+    console.log("useEffect- updating document title..");
     document.title = `You clicked ${count}`;
-  });
+  }, [count]);
 
   return (
     <div>
       <h2>UseEffect Hook</h2>
+      <input placeholder="enter name" value={name} onChange={handleInput} />
+      <br />
       <button onClick={() => setCOunt(count + 1)}>Inc</button>
       <HookCounterTwo />
       <h1>count: {count}</h1>
